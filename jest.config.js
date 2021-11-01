287d06901bd19bd178fb219c02dba8f7ee3cffd3
Isa(yunjongryu) <yunjonglyu@gmail.com>
2021-11-01 13:27:10 +0900

    chore: JEST - 바벨 관련 추가

--- jest.config.js
+++ jest.config.js
@@ -174,7 +174,9 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "\\.js$": "babel-jest",
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
