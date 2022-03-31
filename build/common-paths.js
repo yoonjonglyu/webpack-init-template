9e911bf4c40f41321044656141879f2586f5d865
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-03-31 15:45:16 +0900

    style: 린트 설정에 따른 인덴트 & 텝사이즈 등 스타일 변경

--- build/common-paths.js
+++ build/common-paths.js
@@ -2,7 +2,7 @@ const path = require('path');
 const PROJECT_ROOT = path.resolve(__dirname, '../');
 
 module.exports = {
-    projectRoot : PROJECT_ROOT,
-    outputPath : path.join(PROJECT_ROOT, 'dist'),
-    appEntry : path.join(PROJECT_ROOT, 'src')
+    projectRoot: PROJECT_ROOT,
+    outputPath: path.join(PROJECT_ROOT, 'dist'),
+    appEntry: path.join(PROJECT_ROOT, 'src'),
 };
