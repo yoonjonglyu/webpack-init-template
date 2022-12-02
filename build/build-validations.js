bb6af9b81d54552e09052205665ef1c0733c78e3
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-12-02 13:42:28 +0900

    etc: 파일 최신화

--- build/build-validations.js
+++
@@ -1,8 +0,0 @@
-const chalk = require('chalk');
-const ERR_NO_ENV = chalk.red(
-  `you must pass an --env.env, 웹팩 빌드를 위해서 환경 변수를 포함해주세요!`
-);
-
-module.exports = {
-  ERR_NO_ENV,
-};
--- src/index.test.js
+++
@@ -1,6 +0,0 @@
-describe('init', () => {
-  it('테스트 기초 설정', () => {
-    const sum = (a, b) => a + b;
-    expect(sum(2, 2)).toBe(4);
-  });
-});
